b35650a25636ed280cf86274010dc6477e38220a
Graeme Henderson <graeme.189@gmail.com>
2019-09-10 18:38:40 +0100

    Add default price to Dodgems and methods and tests for normal and under 12s price

--- src/main/java/attractions/Dodgems.java
+++ src/main/java/attractions/Dodgems.java
@@ -1,10 +1,28 @@
 package attractions;
 
 import behaviours.IReviewed;
+import behaviours.ITicketed;
+import people.Visitor;
 
-public class Dodgems extends Attraction implements IReviewed {
+public class Dodgems extends Attraction implements IReviewed, ITicketed {
 
-    public Dodgems(String name, int rating) {
+    double price;
+
+    public Dodgems(String name, int rating, double price) {
         super(name, rating);
+        this.price = price;
+    }
+
+    public double defaultPrice(){
+        return this.price;
+    }
+
+    public double priceFor(Visitor visitor) {
+        if (visitor.getAge() < 12) {
+            return this.price / 2;
+        } else {
+            return this.price;
+        }
+
     }
 }
--- src/test/java/attractions/DodgemTest.java
+++ src/test/java/attractions/DodgemTest.java
@@ -2,18 +2,23 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class DodgemTest {
 
     Dodgems dodgems;
+    Visitor visitor1;
+    Visitor visitor2;
+
 
     @Before
     public void setUp() throws Exception {
-        dodgems = new Dodgems("Bumper Cars", 5);
+        dodgems = new Dodgems("Bumper Cars", 5, 4.50);
+        visitor1 = new Visitor(17, 150, 50);
+        visitor2 = new Visitor(11, 150, 70);
     }
-
 
     @Test
     public void hasName() {
@@ -28,5 +33,15 @@ public class DodgemTest {
     @Test
     public void hasVisitCount() {
         assertEquals(0, dodgems.getVisitCount());
+    }
+
+    @Test
+    public void checkDefaultPrice() {
+        assertEquals(4.50, dodgems.priceFor(visitor1), 0.001);
+    }
+
+    @Test
+    public void checkPriceForUnder12(){
+        assertEquals(2.25, dodgems.priceFor(visitor2), 0.001);
     }
 }
