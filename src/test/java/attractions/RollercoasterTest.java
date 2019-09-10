7911f093849ca610d5313b06b7eda4a6a7d15a8a
Graeme Henderson <graeme.189@gmail.com>
2019-09-10 16:46:06 +0100

    Rollercoaster age and height added and tests passing

--- src/test/java/attractions/RollercoasterTest.java
+++ src/test/java/attractions/RollercoasterTest.java
@@ -2,16 +2,25 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class RollercoasterTest {
 
     RollerCoaster rollerCoaster;
+    Visitor visitor1;
+    Visitor visitor2;
+    Visitor visitor3;
+    Visitor visitor4;
+
 
     @Before
     public void setUp() {
         rollerCoaster = new RollerCoaster("Blue Ridge", 10);
+        visitor1 = new Visitor(17, 150, 50);
+        visitor2 = new Visitor(11, 150, 70);
+        visitor3 = new Visitor(17, 130, 50);
     }
 
     @Test
@@ -27,5 +36,20 @@ public class RollercoasterTest {
     @Test
     public void hasVisitCount() {
         assertEquals(0, rollerCoaster.getVisitCount());
+    }
+
+    @Test
+    public void canVisitRollerCoasterAge() {
+        assertEquals(true, rollerCoaster.isAllowedTo(visitor1));
+    }
+
+    @Test
+    public void cannotVisitRollerCoasterAge() {
+        assertEquals(false, rollerCoaster.isAllowedTo(visitor2));
+    }
+
+    @Test
+    public void cannotVisitRollerCoasterHeight() {
+        assertEquals(false, rollerCoaster.isAllowedTo(visitor3));
     }
 }
